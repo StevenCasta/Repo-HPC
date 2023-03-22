ebab7e54b685e9d38d9ad445bef94dfc927b35ec
Steven Castaño <stevencastano196@gmail.com>
2023-03-22 10:38:07 -0500

    Preparando el script para test

--- script.sh
+++ script.sh
@@ -3,9 +3,12 @@ for j in {1..10}
 do
 for a in 2 4 6
 do
-for i in 10 20 30 40 50 60 70 80 90 100
+for i in 500 600 700 800 900 1000 1100 1200 1300 1400 1500
 do
-./test1 $i $a >> times.doc
+./sequential-matrix $i >> times-sequential-matrix.doc
+./sequential-matrix-transpose $i >> times-sequential-matrix-transpose.doc
+./threads-matrix $i $a >> times-threads-matrix.doc
+./sequential-matrix $i $a >> times-processes-matrix.doc
 done
 done
 done
