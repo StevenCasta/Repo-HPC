98709f57ebf44ff6ab77a5800a4dac0d11d936e2
Steven Castaño <stevencastano196@gmail.com>
2023-03-23 00:01:06 -0500

    Script para correr los códigos por primera vez

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-for j in {1..6} 
+for j in {1..10} 
 do
-for i in 1500 2500 3500 4500 5500 6500
+for i in 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000
 do
 ./sequential-matrix.out $i >> times-sequential-matrix.doc
 ./sequential-matrix-O3.out $i >> times-sequential-matrix-O3.doc
@@ -9,11 +9,11 @@ do
 done
 done
 
-for j in {1..6}
+for j in {1..10}
 do
 for a in 2 4 8 16
 do
-for i in 1500 2500 3500 4500 5500 6500
+for i in 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000
 do
 ./threads-matrix.out $i $a >> times-threads-matrix.doc
 ./processes-matrix.out $i $a >> times-processes-matrix.doc
