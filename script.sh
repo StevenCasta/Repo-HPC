55ca0ec19bb8e5ef5dbbcfcc433db48cb9c23ac0
Steven Castaño <stevencastano196@gmail.com>
2023-03-28 04:08:50 -0500

    Se ejecutó una vez más los códigos secuenciales

--- script.sh
+++ script.sh
@@ -1,22 +1,12 @@
 #! /bin/bash
-for j in {1..10} 
-do
-for i in 1000 1500 2000 2500 3000 3500
-do
-./sequential-matrix.out $i >> times-sequential-matrix.doc
-./sequential-matrix-O3.out $i >> times-sequential-matrix-O3.doc
-./sequential-matrix-transpose.out $i >> times-sequential-matrix-transpose.doc
-done
-done
-
 for j in {1..10}
 do
 for a in 2 4 8 16
 do
-for i in 1000 1500 2000 2500 3000 3500
+for i in 4000 4500
 do
-./threads-matrix.out $i $a >> times-threads-matrix.doc
-./processes-matrix.out $i $a >> times-processes-matrix.doc
+./threads-matrix.out $i $a >> times2-threads-matrix.doc
+./processes-matrix.out $i $a >> times2-processes-matrix.doc
 done
 done
 done
